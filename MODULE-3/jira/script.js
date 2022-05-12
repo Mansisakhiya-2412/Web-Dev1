f940a3213162932cb11d9aa4cd9c80173f23f954
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-05-12 23:00:13 +0530

    UI and delete Functionally added

--- MODULE-3/jira/script.js
+++ MODULE-3/jira/script.js
@@ -3,24 +3,26 @@ let modalCont = document.querySelector(".modal-cont");
 let taskAreaCont = document.querySelector(".textarea-cont");
 let mainCont = document.querySelector(".main-cont");
 let allPriorityColors = document.querySelectorAll(".priority-color");
+let removeBtn = document.querySelector(".remove-btn");
 let addModal = true;
-let modalPriorityColor = 'black';
+let removeFlag = false;
+    let modalPriorityColor = 'black';
 
 addBtn.addEventListener("click", function () {
     if (addModal) {
         //show modal
         modalCont.style.display = "flex";
-    }else{
+    } else {
         //hide modal
         modalCont.style.display = "none";
     }
     addModal = !addModal;
 })
 
-for(let i=0;i<allPriorityColors.length;i++){
+for (let i = 0; i < allPriorityColors.length; i++) {
     let priorityDivOneColor = allPriorityColors[i];
-    priorityDivOneColor.addEventListener("click",function(){        
-        for(let j=0;j<allPriorityColors.length;j++){
+    priorityDivOneColor.addEventListener("click", function () {
+        for (let j = 0; j < allPriorityColors.length; j++) {
             allPriorityColors[j].classList.remove("active");
         }
         priorityDivOneColor.classList.add("active");
@@ -29,9 +31,9 @@ for(let i=0;i<allPriorityColors.length;i++){
 }
 
 
-modalCont.addEventListener("keydown",function(e){
+modalCont.addEventListener("keydown", function (e) {
     let key = e.key;
-    if(key == 'Enter'){
+    if (key == 'Enter') {
         createTicket(modalPriorityColor, taskAreaCont.value);
         taskAreaCont.value = "";
         modalCont.style.display = "none";
@@ -39,16 +41,32 @@ modalCont.addEventListener("keydown",function(e){
     }
 })
 
-function createTicket(ticketColor,task){
+removeBtn.addEventListener("click",function(){
+    if(removeFlag){
+        removeBtn.style.color = 'black'
+    }else{
+        removeBtn.style.color = "red"
+    }
+    removeFlag = !removeFlag;
+})
+
+function createTicket(ticketColor, task) {
     // <div class="ticket-cont">
     //         <div class="ticket-color"></div>
     //         <div class="ticket-id">#qzu03</div>
     //         <div class="task-area">some task</div>
     //     </div>
     let ticketCont = document.createElement("div");
-    ticketCont.setAttribute('class','ticket-cont');
+    ticketCont.setAttribute('class', 'ticket-cont');
     ticketCont.innerHTML = `<div class="ticket-color ${ticketColor}"></div>
     <div class="ticket-id">#qzu03</div>
     <div class="task-area">${task}</div>`
-mainCont.appendChild(ticketCont);
+    mainCont.appendChild(ticketCont);
+    
+    //handling delete 
+    ticketCont.addEventListener("click",function(){
+        if(removeFlag)
+            ticketCont.remove();
+    })
+
 } 
