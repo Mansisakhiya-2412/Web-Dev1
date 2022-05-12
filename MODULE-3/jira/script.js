b71ef625a096f26303c8a6cb7b2f5fc6a6953d19
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-05-12 21:58:44 +0530

    ticket added with color

--- MODULE-3/jira/script.js
+++ MODULE-3/jira/script.js
@@ -2,7 +2,9 @@ let addBtn = document.querySelector(".add-btn");
 let modalCont = document.querySelector(".modal-cont");
 let taskAreaCont = document.querySelector(".textarea-cont");
 let mainCont = document.querySelector(".main-cont");
+let allPriorityColors = document.querySelectorAll(".priority-color");
 let addModal = true;
+let modalPriorityColor = 'black';
 
 addBtn.addEventListener("click", function () {
     if (addModal) {
@@ -10,22 +12,34 @@ addBtn.addEventListener("click", function () {
         modalCont.style.display = "flex";
     }else{
         //hide modal
-        modalCont.style.display = " none";
+        modalCont.style.display = "none";
     }
     addModal = !addModal;
 })
 
+for(let i=0;i<allPriorityColors.length;i++){
+    let priorityDivOneColor = allPriorityColors[i];
+    priorityDivOneColor.addEventListener("click",function(){        
+        for(let j=0;j<allPriorityColors.length;j++){
+            allPriorityColors[j].classList.remove("active");
+        }
+        priorityDivOneColor.classList.add("active");
+        modalPriorityColor = priorityDivOneColor.classList[0];
+    })
+}
+
+
 modalCont.addEventListener("keydown",function(e){
     let key = e.key;
     if(key == 'Enter'){
-        createTicket(taskAreaCont.value);
+        createTicket(modalPriorityColor, taskAreaCont.value);
         taskAreaCont.value = "";
         modalCont.style.display = "none";
         addModal = !addModal;
     }
 })
 
-function createTicket(task){
+function createTicket(ticketColor,task){
     // <div class="ticket-cont">
     //         <div class="ticket-color"></div>
     //         <div class="ticket-id">#qzu03</div>
@@ -33,8 +47,8 @@ function createTicket(task){
     //     </div>
     let ticketCont = document.createElement("div");
     ticketCont.setAttribute('class','ticket-cont');
-    ticketCont.innerHTML = `<div class="ticket-color"></div>
-                            <div class="ticket-id">#qzu03</div>
-                            <div class="task-area">${task}</div>`
-    mainCont.appendChild(ticketCont);
+    ticketCont.innerHTML = `<div class="ticket-color ${ticketColor}"></div>
+    <div class="ticket-id">#qzu03</div>
+    <div class="task-area">${task}</div>`
+mainCont.appendChild(ticketCont);
 } 
