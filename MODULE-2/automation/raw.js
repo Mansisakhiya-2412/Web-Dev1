197bf5593228bddc299e12dd384726258530d4e1
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-09-13 21:39:42 +0530

    Puppeteer browser opened (cromium)

--- MODULE-2/automation/raw.js
+++ MODULE-2/automation/raw.js
@@ -4,24 +4,14 @@ const puppeteer = require('puppeteer');
 //   const browser = await puppeteer.launch();
 //   const page = await browser.newPage();
 //   await page.goto('https://example.com');
-//   await page.screenshot({ path: 'example.png' });
+//   await page.screenshot({path: 'example.png'});
 
 //   await browser.close();
 // })();
 
+let browserPromise = puppeteer.launch({ headless: false});
 
-
-
-let browserPromise = puppeteer.launch({ headless : false }) ;
-
-browserPromise.then(function(browser){
-    console.log("Browser is Opened");
-    let pagePromise = browser.newPage();
-    return pagePromise;
-}).then(function(page){
-    console.log("Page is Opened");
-    let urlPromise = page.goto("https://www.google.com/");
-    return urlPromise;
-}).then(function(){
-    console.log("google is opened")
+browserPromise.then(function(browserInstance){
+    console.log("Browser is opened");
+    let pagePromise = browserInstance.newPage();
 })
